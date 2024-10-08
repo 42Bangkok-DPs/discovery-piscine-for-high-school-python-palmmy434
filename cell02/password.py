2a9f1b0635f8dd0970b0acdfde3920eb0edff93a
palmmy434 <tonpalm1808@gmail.com>
2024-10-08 16:57:18 +0700

    Add files via upload

---
+++ cell02/password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    password = "Python is awesome"
+    entered_password = input("Enter the password: ")
+
+    if entered_password == password:
+        print("ACCESS GRANTED")
+    else:
+        print("ACCESS DENIED")
+
+if __name__ == "__main__":
+    main()
