d608975722e2d6f11c03717783f53fa4255c2559
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:22:16 +0700

    Update up_low.py

--- cell04/up_low.py
+++ cell04/up_low.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
 
 def main():
-    # Ask the user for a string
     user_input = input("Give me a string: ")
 
-    # Swap the case of the string
+
     swapped_case = user_input.swapcase()
 
-    # Display the result
+
     print(swapped_case)
 
 if __name__ == "__main__":
