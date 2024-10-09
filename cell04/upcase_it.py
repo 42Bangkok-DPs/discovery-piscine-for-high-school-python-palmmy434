d781ce82f27846098ea3fe8ffb736c6544d4e53f
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 11:39:55 +0700

    Add files via upload

---
+++ cell04/upcase_it.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    # Prompt the user for a word
+    user_input = input("Give me a word: ")
+    
+    # Convert the input to uppercase
+    uppercased_word = user_input.upper()
+    
+    # Display the uppercase word
+    print(uppercased_word)
+
+if __name__ == "__main__":
+    main()
