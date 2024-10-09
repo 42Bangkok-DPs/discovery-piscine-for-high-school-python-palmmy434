f0d3773c403db0f01c2d415b609d1e7ac6ccf523
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:21:34 +0700

    Update float.py

--- cell04/float.py
+++ cell04/float.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python3
 
 def main():
-    # Ask the user for a number
+ 
     number_input = input("Give me a number: ")
 
-    # Try to convert the input to a float
+   
     try:
         number = float(number_input)
         
-        # Check if the number is an integer
         if number.is_integer():
             print("This number is an integer.")
         else:
