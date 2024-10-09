5e9aeae32e17870d835d0c59b897c716ba0e1963
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:20:27 +0700

    Update to25.py

--- cell03/to25.py
+++ cell03/to25.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python3
+
 
 def main():
-    # Prompt the user for input
+   
     user_input = input("Enter a number less than 25\n")
     
     try:
-        # Convert input to an integer
+      
         number = int(user_input)
 
-        # Check if the number is greater than 25
+
         if number > 25:
             print("Error")
         else:
-            # Loop from the input number to 25
+         
             for i in range(number, 26):
                 print(f"Inside the loop, my variable is {i}")
 
