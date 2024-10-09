8c18a67b35e25c8f52ec6655973574cfe8e144ce
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:18:52 +0700

    Update mult.py

--- cell02/mult.py
+++ cell02/mult.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python3
+
 
 def main():
-    # Prompt the user for two numbers
+    
     first_number = float(input("Enter the first number:\n"))
     second_number = float(input("Enter the second number:\n"))
     
-    # Calculate the multiplication
+  
     result = first_number * second_number
     
-    # Display the result of the multiplication
     print(f"{first_number} x {second_number} = {result}")
     
-    # Determine if the result is positive, negative, or zero
+   
     if result > 0:
         print("The result is positive.")
     elif result < 0:
