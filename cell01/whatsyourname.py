dc776dfb060852845b4643d813cae974f4be95d7
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:37:22 +0700

    Update whatsyourname.py

--- cell01/whatsyourname.py
+++ cell01/whatsyourname.py
@@ -1,7 +1,5 @@
 first_name = input("Hey, what's your first name? : ")
 
-# Asking for the user's last name
 last_name = input("And your last name? : ")
 
-# Displaying the full name
 print(f"Well, pleased to meet you, {first_name} {last_name}.")
