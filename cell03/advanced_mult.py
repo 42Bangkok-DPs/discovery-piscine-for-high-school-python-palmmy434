bcf73e7aa3ff5fd6d45dc34e749a0d040c967442
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:19:29 +0700

    Update advanced_mult.py

--- cell03/advanced_mult.py
+++ cell03/advanced_mult.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python3
+
 
 def main():
-    # Start from the first multiplication table
+   
     i = 0
 
-    # Outer loop for each multiplication table
+    
     while i <= 10:
         print(f"Table de {i}:", end=" ")
 
-        # Inner loop for multiplication values
         j = 0
         while j <= 10:
             print(i * j, end=" ")
             j += 1
             
-        print()  # Move to the next line
+        print()  
         i += 1
 
 if __name__ == "__main__":
