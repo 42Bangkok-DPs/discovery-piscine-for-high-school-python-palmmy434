d1f81c4f5dc08d1ebaae0474b28eec17087dccb6
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:19:47 +0700

    Update i_got_that.py

--- cell03/i_got_that.py
+++ cell03/i_got_that.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python3
+
 
 def main():
     while True:
-        # Prompt the user for input
+      
         user_input = input("What you gotta say? : ")
         
-        # Check if the input is "STOP"
+      
         if user_input.upper() == "STOP":
             break
         
-        # Respond to the input
         print("I got that! Anything else? : ")
 
 if __name__ == "__main__":
