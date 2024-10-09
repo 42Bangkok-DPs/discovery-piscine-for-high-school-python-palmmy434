80490a65ceeabe44c5904013b7951e1bcc626680
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:20:53 +0700

    Update age.py

--- cell04/age.py
+++ cell04/age.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
+
 
 def main():
-    # Ask the user for their age
     age_input = input("Please tell me your age: ")
     
-    # Convert the input to an integer
+ 
     age = int(age_input)
 
-    # Display the current age and future ages
+   
     print(f"You are currently {age} years old.")
     print(f"In 10 years, you'll be {age + 10} years old.")
     print(f"In 20 years, you'll be {age + 20} years old.")
