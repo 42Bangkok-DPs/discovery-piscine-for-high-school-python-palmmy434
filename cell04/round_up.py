127ecd545e7a866f8e4b4299c0fa36f811b91ed8
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:21:58 +0700

    Update round_up.py

--- cell04/round_up.py
+++ cell04/round_up.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python3
+
 import math
 
 def main():
-    # Ask the user for a number
+    
     number_input = input("Give me a number: ")
 
-    # Try to convert the input to a float
+
     try:
         number = float(number_input)
         
-        # Round up the number
+      
         rounded_number = math.ceil(number)
-        
-        # Display the rounded number
+    
         print(rounded_number)
     except ValueError:
         print("That's not a valid number.")
