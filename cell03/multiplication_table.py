b2937a5da7cb05662981f4869e668469d3e4e65c
palmmy434 <tonpalm1808@gmail.com>
2024-10-09 16:20:05 +0700

    Update multiplication_table.py

--- cell03/multiplication_table.py
+++ cell03/multiplication_table.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python3
+
 
 def main():
-    # Prompt the user for input
+
     user_input = input("Enter a number\n")
     
     try:
-        # Convert input to an integer
         number = int(user_input)
 
-        # Generate and display the multiplication table
         for i in range(10):
             print(f"{i} x {number} = {i * number}")
 
